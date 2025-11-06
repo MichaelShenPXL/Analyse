1211d583167ed5edeffce996ca35b79f7c6e4239
Michael Shen <michael.shen@student.pxl.be>
2025-11-06 13:31:53 +0100

    demo 3

--- analyze/analyze/Program.cs
+++ analyze/analyze/Program.cs
@@ -1,14 +1,25 @@
-﻿namespace analyze
+﻿using System.Reflection.Metadata;
+
+namespace analyze
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string user = "admin", password = "password123";
+
+
+
             Console.Write("geef je gebruikersnaam in: ");
             string Name = Console.ReadLine();
 
             Console.Write("geef nu je wachtwoord in; ");
-            string Password = Console.ReadLine();
+            string wachtwoord = Console.ReadLine();
+
+            if (Name == user && wachtwoord == password)
+            {
+
+            }
         }
     }
 }
